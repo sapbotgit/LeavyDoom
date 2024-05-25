b169ae8ed683c6a7449f4d88b0b958a3a94db36d
sapbotgit <120243584+sapbotgit@users.noreply.github.com>
2024-05-25 22:52:36 +0500

    Create BuildIt.sh

---
+++ doomgeneric/BuildIt.sh
@@ -0,0 +1,8 @@
+echo Building your train to hell!
+make
+make -f Makefile.djgpp
+make -f Makefile.emscripten
+make -f Makefile.freebsd
+make -f Makefile.sdl
+make -f Makefile.soso
+make -f Makefile.sosox
