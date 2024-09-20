f6ad8d598ac9db63e9e2e2dd2aa0caa8d62ad914
sapbotgit <120243584+sapbotgit@users.noreply.github.com>
2024-09-20 21:36:54 +0500

    ChatGPT 4o update lol

--- linuxdoom-1.10/i_video.c
+++ linuxdoom-1.10/i_video.c
@@ -349,175 +349,106 @@ void I_UpdateNoBlit (void)
 //
 // I_FinishUpdate
 //
-void I_FinishUpdate (void)
-{
+void I_FinishUpdate(void) {
+    static int lastTic;
+    int tics, i;
 
-    static int	lasttic;
-    int		tics;
-    int		i;
-    // UNUSED static unsigned char *bigscreen=0;
+    // Draw little dots on the bottom of the screen (debug mode)
+    if (devparm) {
+        i = I_GetTime();
+        tics = i - lastTic;
+        lastTic = i;
+        if (tics > 20) tics = 20;
 
-    // draws little dots on the bottom of the screen
-    if (devparm)
-    {
-
-	i = I_GetTime();
-	tics = i - lasttic;
-	lasttic = i;
-	if (tics > 20) tics = 20;
-
-	for (i=0 ; i<tics*2 ; i+=2)
-	    screens[0][ (SCREENHEIGHT-1)*SCREENWIDTH + i] = 0xff;
-	for ( ; i<20*2 ; i+=2)
-	    screens[0][ (SCREENHEIGHT-1)*SCREENWIDTH + i] = 0x0;
-    
+        for (i = 0; i < tics * 2; i += 2) {
+            screens[0][(SCREENHEIGHT - 1) * SCREENWIDTH + i] = 0xff;
+        }
+        for (; i < 20 * 2; i += 2) {
+            screens[0][(SCREENHEIGHT - 1) * SCREENWIDTH + i] = 0x0;
+        }
     }
 
-    // scales the screen size before blitting it
-    if (multiply == 2)
-    {
-	unsigned int *olineptrs[2];
-	unsigned int *ilineptr;
-	int x, y, i;
-	unsigned int twoopixels;
-	unsigned int twomoreopixels;
-	unsigned int fouripixels;
+    // Scale the screen size before blitting it
+    if (multiply == 2) {
+        scaleScreen(2);
+    } else if (multiply == 3) {
+        scaleScreen(3);
+    } else if (multiply == 4) {
+        // Broken. Gotta fix this some day.
+        Expand4((unsigned *)(screens[0]), (double *) (image->data));
+    }
 
-	ilineptr = (unsigned int *) (screens[0]);
-	for (i=0 ; i<2 ; i++)
-	    olineptrs[i] = (unsigned int *) &image->data[i*X_width];
+    // Update the X11 display
+    if (doShm) {
+        updateXShm();
+    } else {
+        updateX11();
+    }
+}
 
-	y = SCREENHEIGHT;
-	while (y--)
-	{
-	    x = SCREENWIDTH;
-	    do
-	    {
-		fouripixels = *ilineptr++;
-		twoopixels =	(fouripixels & 0xff000000)
-		    |	((fouripixels>>8) & 0xffff00)
-		    |	((fouripixels>>16) & 0xff);
-		twomoreopixels =	((fouripixels<<16) & 0xff000000)
-		    |	((fouripixels<<8) & 0xffff00)
-		    |	(fouripixels & 0xff);
+void scaleScreen(int scaleFactor) {
+    unsigned int *ilineptr;
+    unsigned int *olineptrs[scaleFactor];
+    int x, y, i;
+    unsigned int pixels[scaleFactor];
+
+    ilineptr = (unsigned int *)(screens[0]);
+    for (i = 0; i < scaleFactor; i++) {
+        olineptrs[i] = (unsigned int *)&(image->data[i * X_width]);
+    }
+
+    y = SCREENHEIGHT;
+    while (y--) {
+        x = SCREENWIDTH;
+        do {
+            unsigned int fouripixels = *ilineptr++;
+            for (i = 0; i < scaleFactor; i++) {
+                switch (scaleFactor) {
+                    case 2:
+                        pixels[i] = (i == 0)
+                                ? ((fouripixels & 0xff000000) | ((fouripixels >> 8) & 0xffff00) | ((fouripixels >> 16) & 0xff))
+                                : ((fouripixels << 16) & 0xff000000) | ((fouripixels << 8) & 0xffff00) | (fouripixels & 0xff);
+                        break;
+                    case 3:
+                        pixels[i] = (i == 0)
+                                ? ((fouripixels & 0xff000000) | ((fouripixels >> 8) & 0xff0000) | ((fouripixels >> 16) & 0xffff))
+                                : (i == 1)
+                                  ? ((fouripixels << 8) & 0xff000000) | (fouripixels & 0xffff00) | ((fouripixels >> 8) & 0xff)
+                                  : ((fouripixels << 16) & 0xffff0000) | ((fouripixels << 8) & 0xff00) | (fouripixels & 0xff);
+                        break;
+                    default:
+                        break;
+                }
+            }
 #ifdef __BIG_ENDIAN__
-		*olineptrs[0]++ = twoopixels;
-		*olineptrs[1]++ = twoopixels;
-		*olineptrs[0]++ = twomoreopixels;
-		*olineptrs[1]++ = twomoreopixels;
+            for (i = 0; i < scaleFactor; i++) {
+                *olineptrs[i]++ = pixels[i];
+            }
 #else
-		*olineptrs[0]++ = twomoreopixels;
-		*olineptrs[1]++ = twomoreopixels;
-		*olineptrs[0]++ = twoopixels;
-		*olineptrs[1]++ = twoopixels;
+            for (i = scaleFactor - 1; i >= 0; i--) {
+                *olineptrs[i]++ = pixels[i];
+            }
 #endif
-	    } while (x-=4);
-	    olineptrs[0] += X_width/4;
-	    olineptrs[1] += X_width/4;
-	}
-
+        } while (x -= 4);
+        for (i = 0; i < scaleFactor; i++) {
+            olineptrs[i] += X_width / 4;
+        }
     }
-    else if (multiply == 3)
-    {
-	unsigned int *olineptrs[3];
-	unsigned int *ilineptr;
-	int x, y, i;
-	unsigned int fouropixels[3];
-	unsigned int fouripixels;
+}
 
-	ilineptr = (unsigned int *) (screens[0]);
-	for (i=0 ; i<3 ; i++)
-	    olineptrs[i] = (unsigned int *) &image->data[i*X_width];
-
-	y = SCREENHEIGHT;
-	while (y--)
-	{
-	    x = SCREENWIDTH;
-	    do
-	    {
-		fouripixels = *ilineptr++;
-		fouropixels[0] = (fouripixels & 0xff000000)
-		    |	((fouripixels>>8) & 0xff0000)
-		    |	((fouripixels>>16) & 0xffff);
-		fouropixels[1] = ((fouripixels<<8) & 0xff000000)
-		    |	(fouripixels & 0xffff00)
-		    |	((fouripixels>>8) & 0xff);
-		fouropixels[2] = ((fouripixels<<16) & 0xffff0000)
-		    |	((fouripixels<<8) & 0xff00)
-		    |	(fouripixels & 0xff);
-#ifdef __BIG_ENDIAN__
-		*olineptrs[0]++ = fouropixels[0];
-		*olineptrs[1]++ = fouropixels[0];
-		*olineptrs[2]++ = fouropixels[0];
-		*olineptrs[0]++ = fouropixels[1];
-		*olineptrs[1]++ = fouropixels[1];
-		*olineptrs[2]++ = fouropixels[1];
-		*olineptrs[0]++ = fouropixels[2];
-		*olineptrs[1]++ = fouropixels[2];
-		*olineptrs[2]++ = fouropixels[2];
-#else
-		*olineptrs[0]++ = fouropixels[2];
-		*olineptrs[1]++ = fouropixels[2];
-		*olineptrs[2]++ = fouropixels[2];
-		*olineptrs[0]++ = fouropixels[1];
-		*olineptrs[1]++ = fouropixels[1];
-		*olineptrs[2]++ = fouropixels[1];
-		*olineptrs[0]++ = fouropixels[0];
-		*olineptrs[1]++ = fouropixels[0];
-		*olineptrs[2]++ = fouropixels[0];
-#endif
-	    } while (x-=4);
-	    olineptrs[0] += 2*X_width/4;
-	    olineptrs[1] += 2*X_width/4;
-	    olineptrs[2] += 2*X_width/4;
-	}
-
+void updateXShm(void) {
+    if (!XShmPutImage(X_display, X_mainWindow, X_gc, image, 0, 0, 0, 0, X_width, X_height, True)) {
+        I_Error("XShmPutImage() failed\n");
     }
-    else if (multiply == 4)
-    {
-	// Broken. Gotta fix this some day.
-	void Expand4(unsigned *, double *);
-  	Expand4 ((unsigned *)(screens[0]), (double *) (image->data));
-    }
+    shmFinished = false;
+    do {
+        I_GetEvent();
+    } while (!shmFinished);
+}
 
-    if (doShm)
-    {
-
-	if (!XShmPutImage(	X_display,
-				X_mainWindow,
-				X_gc,
-				image,
-				0, 0,
-				0, 0,
-				X_width, X_height,
-				True ))
-	    I_Error("XShmPutImage() failed\n");
-
-	// wait for it to finish and processes all input events
-	shmFinished = false;
-	do
-	{
-	    I_GetEvent();
-	} while (!shmFinished);
-
-    }
-    else
-    {
-
-	// draw the image
-	XPutImage(	X_display,
-			X_mainWindow,
-			X_gc,
-			image,
-			0, 0,
-			0, 0,
-			X_width, X_height );
-
-	// sync up with server
-	XSync(X_display, False);
-
-    }
-
+void updateX11(void) {
+    XPutImage(X_display, X_mainWindow, X_gc, image, 0, 0, 0, 0, X_width, X_height);
+    XSync(X_display, False);
 }
 
 
